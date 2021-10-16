c36423094ac22b885d1da5fefcf991a9c46cf2c8
Phats Lys <lhpworking@gamil.com>
2021-10-16 17:56:37 +0700

    fix some error

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,17 +4,17 @@ var i;
 
 faqBtns.forEach((faqBtn) => {
     faqBtn.addEventListener("click", () => {
-      if (faqBtn.classList.contains("isActive")) {
-        faqBtn.classList.remove("isActive");
-      } else {
-        const faqBtnWithIsActive = document.querySelectorAll(".faq-btn");
-        faqBtnWithIsActive.forEach((faqBtnWithIsActive) => {
-            faqBtnWithIsActive.classList.remove("isActive");
-        });
-        faqBtn.classList.add("isActive");
-      }
+        if (faqBtn.classList.contains("isActive")) {
+            faqBtn.classList.remove("isActive");
+        } else {
+            const faqBtnWithIsActive = document.querySelectorAll(".faq-btn");
+            faqBtnWithIsActive.forEach((faqBtnWithIsActive) => {
+                faqBtnWithIsActive.classList.remove("isActive");
+            });
+            faqBtn.classList.add("isActive");
+        }
     });
-  });
+});
 // for (i = 0; i < faqBtn.length; i++) {
 //     faqBtn[i].addEventListener("click", function () {
 //         this.classList.toggle("isActive");
